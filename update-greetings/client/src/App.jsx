59469343eb26c5ad738d7042826f78e5965cb91f
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-22 12:08:01 +0200

    add state for the current greetings

--- update-greetings/client/src/App.jsx
+++ update-greetings/client/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Web3 from 'web3';
 // import Contract from 'web3-eth-contract'
 import { contractAbi, contractAddress } from './utils/constants';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // I had to downgrade react scripts to 4.0.3 from 5.0.1
 // to import web 3. It is because of the webpack versions
@@ -17,24 +17,30 @@ import { useState } from 'react';
 
 // using local node
 const web3 = new Web3("ws://localhost:8545")
-
 const greeterContract = new web3.eth.Contract(contractAbi, contractAddress);
 
 
 const App = () => {
   const [newGreetings, setNewGreetings] = useState("");
+  const [greetings, setGreetings] = useState("")
 
   // console.log(web3)
+  useEffect(() => async () => {
+    const greetMsg = await greetMe()
+    setGreetings(greetMsg);
+  }, [])
+
 
   const greetMe = async () => {
     const greetMsg = await greeterContract.methods.greet().call()
-    console.log(greetMsg)
+    return greetMsg;
   }
 
   const updateGreets = async () => {
     // if i want to console log
     const greetMsg = await greeterContract.methods.setGreeting(newGreetings).send({ from: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266' })
     console.log(greetMsg)
+    setGreetings(await greetMe())
     // await greeterContract.methods.setGreeting(newGreetings).send()
   }
 
@@ -48,6 +54,7 @@ const App = () => {
       <button onClick={() => updateGreets()}>
         Update Greetings
       </button>
+      <h3>Current Greetings: {greetings}</h3>
     </div>
   );
 }
